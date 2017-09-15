57db6c3aa90f177a912358540cf8f481e9052326
neilgao26 <neilgao26@outlook.com>
2017-09-15 11:12:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/implicitintentpractice/AnotherActivity.java
@@ -0,0 +1,16 @@
+package com.example.implicitintentpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/9/15.
+ */
+
+public class AnotherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
